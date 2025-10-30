656d0fd6fefd7168af3d5e7ebc5401b77d9df608
poguk12 <okrodik@gmail.com>
2025-10-31 08:52:07 +0900

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def sum(a, b):
-    return(a+b)
+import sum
 
 def nesum(a, b):
-    return(a+b)
+    return(a - b)
 
-print(sum(10, 20))
+def umnozenie(a, b):
+    return (a * b)
+
+print(sum.sum(10, 20))
 print(nesum(10, 20))
+print(umnozenie(10, 20))
